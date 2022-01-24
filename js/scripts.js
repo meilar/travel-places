5e60c1bf8bd944305a7809f7128057a26ad9b4d2
Matt Eilar <meilar@gmail.com>
2022-01-24 10:52:49 -0800

    add UI logic
    Co-authored-by: Matt Eilar <meilar@gmail.com>
    Co-Authored-by Ryan Bass <theryanbass@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,8 @@ Passport.prototype.assignId = function() {
   return this.currentId;
 }
 
+let userPassport = new Passport();
+
 // BL 4 Trips
 
 function Trip(location, landmark, yearTime, note) {
@@ -26,6 +28,33 @@ Trip.prototype.tripReport = function() {
   return "I visited " + this.location + ". It was beautiful in " + this.yearTime + ". If you go, make sure to visit " + this.landmark + ". Make sure to remember: " + this.note + "! Have fun!";
 }
 
+function listTrip() {
+  let index = userPassport.currentId;
+  let tripList = userPassport.trips[index].tripReport();
+  return tripList;
+}
+
+function getTrip() {
+  let locationInput = $('#locationId').val();
+  let yearTimeInput = $("#yearTimeId").val();
+  let landmarkInput = $("#landmarkId").val();
+  let noteInput = $("#noteId").val();
+  let outputTrip = new Trip(locationInput, yearTimeInput, landmarkInput, noteInput);
+  userPassport.addTrip(outputTrip);
+  console.log(userPassport);
+}
+
 // User Interface
 
+$(document).ready(function() {
+
+  $("#tripInput").submit(function(event){
+    event.preventDefault();
+    getTrip();
+    outputTripList = listTrip();
+    $("#output-list").append("<li class='output-item'>" + outputTripList + "</li>");
+
+  })
+
+})
 
